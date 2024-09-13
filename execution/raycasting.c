2e14897242ff147af5bfa129a3a308750aeb0827
Suhana Sajid <73544349+suhana66@users.noreply.github.com>
2024-09-13 12:16:00 +0400

    hello world

--- execution/raycasting.c
+++ execution/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: susajid <susajid@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 16:24:39 by susajid           #+#    #+#             */
-/*   Updated: 2024/09/13 11:49:23 by susajid          ###   ########.fr       */
+/*   Updated: 2024/09/13 12:11:12 by susajid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,9 @@ void	draw_3d_rays(t_display *display)
 	r = 0;
 	while (r < WIN_WIDTH)
 	{
-		dist_v = v_line_check(display->player, display->map, display->map_height, &ray);
+		dist_v = v_line_check(display, &ray);
 		v_y = ray.y;
-		dist_h = h_line_check(display->player, display->map, display->map_height, &ray);
+		dist_h = h_line_check(display, &ray);
 		cur_img = get_image(display, ray.a, dist_v, dist_h);
 		if (dist_v < dist_h)
 			x_image = (double)((int)v_y % CUBE_SIZE) / CUBE_SIZE * cur_img.w;
@@ -83,52 +83,48 @@ void	draw_column(t_image *img, double dist, int r, int x_image, t_image cur_img)
 	}
 }
 
-double	h_line_check(t_coord player, char **map, int map_height, t_coord *ray)
+double	h_line_check(t_display *display, t_coord *ray)
 {
 	double	trig_ra;
 
 	trig_ra = 1.0 / tan(deg_to_rad(ray->a));
-	ray->x = player.x;
-	ray->y = player.y;
 	if (sin(deg_to_rad(ray->a)) > 0.001)
 	{
-		ray->y = (int)player.y / CUBE_SIZE * CUBE_SIZE - 0.0001;
+		ray->y = (int)display->player.y / CUBE_SIZE * CUBE_SIZE - 0.0001;
 		ray->dy = -CUBE_SIZE;
 	}
 	else if (sin(deg_to_rad(ray->a)) < -0.001)
 	{
-		ray->y = (int)player.y / CUBE_SIZE * CUBE_SIZE + CUBE_SIZE;
+		ray->y = (int)display->player.y / CUBE_SIZE * CUBE_SIZE + CUBE_SIZE;
 		ray->dy = CUBE_SIZE;
 	}
 	else
 		return (INFINITY);
-	ray->x = (player.y - ray->y) * trig_ra + player.x;
+	ray->x = (display->player.y - ray->y) * trig_ra + display->player.x;
 	ray->dx = -ray->dy * trig_ra;
-	return (calc_dist(player, map, map_height, ray));
+	return (calc_dist(display->player, display->map, display->map_height, ray));
 }
 
-double	v_line_check(t_coord player, char **map, int map_height, t_coord *ray)
+double	v_line_check(t_display *display, t_coord *ray)
 {
 	double	trig_ra;
 
 	trig_ra = tan(deg_to_rad(ray->a));
-	ray->x = player.x;
-	ray->y = player.y;
 	if (cos(deg_to_rad(ray->a)) > 0.001)
 	{
-		ray->x = (int)player.x / CUBE_SIZE * CUBE_SIZE + CUBE_SIZE;
+		ray->x = (int)display->player.x / CUBE_SIZE * CUBE_SIZE + CUBE_SIZE;
 		ray->dx = CUBE_SIZE;
 	}
 	else if (cos(deg_to_rad(ray->a)) < -0.001)
 	{
-		ray->x = (int)player.x / CUBE_SIZE * CUBE_SIZE - 0.0001;
+		ray->x = (int)display->player.x / CUBE_SIZE * CUBE_SIZE - 0.0001;
 		ray->dx = -CUBE_SIZE;
 	}
 	else
 		return (INFINITY);
-	ray->y = (player.x - ray->x) * trig_ra + player.y;
+	ray->y = (display->player.x - ray->x) * trig_ra + display->player.y;
 	ray->dy = -ray->dx * trig_ra;
-	return (calc_dist(player, map, map_height, ray));
+	return (calc_dist(display->player, display->map, display->map_height, ray));
 }
 
 double	calc_dist(t_coord player, char **map, int map_height, t_coord *ray)
